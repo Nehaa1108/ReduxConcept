52955b97ea5e96524cb98cd4a2204d322573bf7e
Nehaa1108 <127198995+Nehaa1108@users.noreply.github.com>
2025-06-22 04:07:08 -0400

    added

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,13 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import { CounterSlice } from "../features/CounterSlice";
-// export const store = configureStore({
-//   reducer : {
-//     counter : CounterSlice
-//   },
-// })
-
-
-
 import counterReducer from "../features/CounterSlice";
 
 export const store = configureStore({
