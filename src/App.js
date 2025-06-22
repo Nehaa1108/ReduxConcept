c0e322d98b593e4356da4a6825db0080362f8049
Nehaa1108 <127198995+Nehaa1108@users.noreply.github.com>
2025-06-22 02:25:56 -0400

    created Counter file and edit existing file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './component/Counter';
 
 function App() {
   return (
-    <div className="App">
-      app
-    </div>
+    <>
+    <Counter/>
+    </>
   );
 }
 
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { counterReducer } from "../features/CounterSlice";
+import { CounterSlice } from "../features/CounterSlice";
 export const store = configureStore({
   reducer : {
-    counter : counterReducer
+    counter : CounterSlice
   },
 })
